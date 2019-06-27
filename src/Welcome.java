69ec02f3893a268da0c3decb8ca0e8daec0b92bd
Jason <jasongenekwak@gmail.com>
2019-06-26 22:28:24 -0400

    tomato and eggs

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,5 +2,6 @@
 public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Hello World again!");
+		System.out.println("Your mom doesn't make good tomato and eggs!");
 	}
 }
